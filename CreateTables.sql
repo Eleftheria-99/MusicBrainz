b43efa9e33c1cc3d4e0b3a93c0406135b8da6bcc
Eleftheria <it21754@hua.gr>
2020-03-30 23:17:16 +0300

    Add files via upload

---
+++ CreateTables.sql
@@ -0,0 +1,15 @@
+create table Artist(
+      artist_type varchar2(100) default null,
+      artist_name varchar2(100)  default null, 
+      artist_gender varchar2(100)  default null,
+      artist_country varchar2(100)  default null
+      );
+     
+create table Releases( 
+      Release_title varchar2(100)  default null,
+      Release_status varchar2(100) default null,
+      Release_date varchar2(100)  default null,
+      Release_format varchar2(100)  default null,
+      Release_country varchar2(100)  default null
+         
+);
